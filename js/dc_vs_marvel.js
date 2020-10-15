52e9bc9944ee63cde4bee6085f04b49185967d9d
Patrick Beltran <66067267+pgbeltran15@users.noreply.github.com>
2020-10-15 19:49:53 -0400

    Update dc_vs_marvel.js

--- js/dc_vs_marvel.js
+++ js/dc_vs_marvel.js
@@ -1,6 +1,5 @@
-// Int Filter
 function IntFilter(heroes) {
-    d3.csv("./data/powerstats.csv").then(function(herodata){
+    d3.csv("/data/powerstats.csv").then(function(herodata){
         
         //filter DC Comics and Marvel Comics heroes/villains
         var dc_data = herodata.filter(function (hero){
@@ -32,22 +31,54 @@ function IntFilter(heroes) {
             type: 'bar',
             x: reversed_DC_Int.map(row => row.name),
             y: reversed_DC_Int.map(row => row.intelligence_score),
+            marker: {color: 'darkblue'}
+
         }
 
+        DCLayout = {
+            plot_bgcolor: 'white',
+            paper_bgcolor:"#2b3e50",
+            xaxis : {
+                  color : 'white',
+                  linecolor: 'white',
+                  title: 'DC Comics Heroes/Villains'
+                },
+            yaxis : {
+                    color : 'white',
+                    linecolor: 'white'
+                  }
+        }
+    
         DC_data = [DC_Trace]
 
-        Plotly.newPlot('bar', DC_data);
+        Plotly.newPlot('bar', DC_data, DCLayout);
 
         Marvel_Trace = {
             type: 'bar',
             x: reversed_marvel_Int.map(row => row.name),
             y: reversed_marvel_Int.map(row => row.intelligence_score),
+            marker: {color: 'darkred'}
+        }
+
+        MarvelLayout = {
+            plot_bgcolor: 'white',
+            paper_bgcolor:"#2b3e50",
+            xaxis : {
+                  color : 'white',
+                  linecolor: 'white',
+                  title: 'Marvel Comics Heroes/Villains'
+                },
+            yaxis : {
+                    color : 'white',
+                    linecolor: 'white'
+                  }
         }
 
         Marvel_data = [Marvel_Trace]
 
-        Plotly.newPlot('bar2', Marvel_data);
+        Plotly.newPlot('bar2', Marvel_data, MarvelLayout);
 
     }); //end of d3 function
 };
+
 IntFilter()
