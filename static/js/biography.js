8648133d9a4a20d3c7812410a8885be7fb97cb3f
John J. Falcone <66309588+Apex8@users.noreply.github.com>
2020-10-17 11:59:31 -0400

    Update biography.js

--- static/js/biography.js
+++ static/js/biography.js
@@ -1,2 +1,19 @@
-d3.csv("/data/biography.csv").then(function(data) {
+d3.csv("/data/biography.csv", function(d) {
+  return {
+    name : d.name,
+    real_name : d.real_name,
+    history_text : d.history_text,
+    powers_text : d.powers_text,
+    place_of_birth : d.place_of_birth,
+    alignment : d.alignment,
+    gender : d.gender,
+    type_race : d.type_race,
+    height : d.height,
+    weight: d.weight,
+    eye_color : d.eye_color,
+    hair_color : d.hair_color,
+    skin_color : d.skin_color
+  };
+}).then(function(data) {
   console.log(data[0]);
+});
