7731f530dce20ad0fef8409f74b8c85d26b8634d
drewrya <66854565+drewrya@users.noreply.github.com>
2020-10-21 20:41:08 -0400

    added in new JS library
    added in a new JS library and update the html and css to adjust it

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,88 @@
-// from data.js
-var tableData = data;
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 2;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
 
-// Select the button
-var button = d3.select("#filter-btn");
 
-// Select the form
-var form = d3.select("#form");
-
-// Get a reference to the table body
-var tbody = d3.select("tbody");
-
-// Create event handlers 
-button.on("click", runEnter);
-form.on("submit",runEnter);
-
-// // Use d3 to update each cell's text with
-// // ufo values ( `date/time`, `city`, `state`, `country`, `shape`, and `comment`)
-// data.forEach(function(ufoSightings) {
-//   console.log(ufoSightings);
-//   var row = tbody.append("tr");
-//   Object.entries(ufoSightings).forEach(function([key, value]) {
-//     // Append a cell to the row for each value
-//     // in the ufo object
-//     var cell = row.append("td");
-//     cell.text(value);
-//   });
-// });
-
-// Complete the event handler function for the form
-function runEnter() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-    
-    // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
-  
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    var filteredData = tableData.filter(date => date.datetime === inputValue);
-    
-    // Then, select the unordered list element by class name
-    var tbody = d3.select("tbody");
-
-    // remove any children from the list to
-    tbody.html("");
-
-    //Update the form with the new filtered data
-    filteredData.forEach(function(filtered) {
-        console.log(filtered);
-        var row = tbody.append("tr");
-        Object.entries(filtered).forEach(function([key, value]) {
-          var cell = row.append("td");
-          cell.text(value);
-        });
-    });
-};
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-4',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-5',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-6',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
